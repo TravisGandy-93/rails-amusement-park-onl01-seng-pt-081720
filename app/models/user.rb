75f94fbe8e89018e9629507059a245f7bf1dae35
Travis Gandy <travis_gandy@yahoo.com>
2021-03-07 18:29:29 +0000

    Automatically backed up by Learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,21 @@
 class User < ActiveRecord::Base
+  has_secure_password
+
+  has_many :rides
+  has_many :attractions, through: :rides
+
+  
+  def mood
+    unless nausea.nil? || happiness.nil?
+      if nausea > happiness
+        "sad"
+      elsif happiness > nausea
+        "happy"
+      end
+    end
+  end
+
+  def admin?
+    admin
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :attractions, :rides, :users, :sessions
+
+  get '/signin', to: 'sessions#new'
+
+  delete '/signout', to: 'sessions#destroy'
+
+  root to: "sessions#splash"
 end
